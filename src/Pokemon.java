8cb5ad640ca4f6b20644170f499d10245b06847f
batsoarr000 <batsoarr000@CAM-LAB-10.bgsd.k12.wa.us>
2019-09-09 07:24:11 -0700

    pokemon

--- src/Pokemon.java
+++ src/Pokemon.java
@@ -3,7 +3,9 @@ public class Pokemon {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-	}
+		System.out.println("He welcomes you");
+		System.out.println(" \\_/ ");
+		System.out.println("o^.^");
+		}
 
 }
